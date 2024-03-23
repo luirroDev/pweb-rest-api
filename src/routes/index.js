bda29308f95ea14f4b5115e6b44b269bfa4aa21d
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2024-03-23 15:39:39 -0400

    added tratamiento to index.router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import expedientesRouter from './expediente.route.js';
 import ordenIngresoRouter from './orden-ingreso.route.js';
+import tratamientoRouter from './tratamiento.route.js';
 
 function routerAPI(app) {
   const router = Router();
   app.use('/api/v1', router);
   router.use('/expedientes', expedientesRouter);
   router.use('/orden-ingreso', ordenIngresoRouter);
+  router.use('/tratamiento', tratamientoRouter);
 }
 
 export default routerAPI;
