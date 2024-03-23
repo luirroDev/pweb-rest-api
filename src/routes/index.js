0b131c0591072bf3a306b088a60de41155329f48
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2024-03-23 13:09:24 -0400

    added orden-ingreso Router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import expedientesRouter from './expediente.route.js';
+import ordenIngresoRouter from './orden-ingreso.route.js';
 
 function routerAPI(app) {
   const router = Router();
   app.use('/api/v1', router);
   router.use('/expedientes', expedientesRouter);
+  router.use('/orden-ingreso', ordenIngresoRouter);
 }
 
 export default routerAPI;
