d6296f243133f752e01c9f6e568dd7c0bba3b45e
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2024-03-26 22:11:38 -0400

    added get profile with acces_token

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import passport from 'passport';
+import userService from '../services/user.service.js';
 import { signToken } from '../auth/jwt.js';
 
 const router = express.Router();
+const userSrv = new userService();
 
 router.post(
   '/login',
@@ -14,6 +16,20 @@ router.post(
       res.json({
         access_token,
       });
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+router.get(
+  '/profile',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res, next) => {
+    try {
+      const userId = req.user.sub;
+      const user = await userSrv.findOne(userId);
+      res.json(user);
     } catch (error) {
       next(error);
     }
